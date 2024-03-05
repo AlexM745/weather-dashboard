f0c2d2aa71ccbb6d6ca5d85dba9dde1dbd759d6a
Alexis Merino <alexismerino49@gmail.com>
2024-03-04 18:41:05 -0600

    added weather format on HTML and got api call to work properly

--- assets/script.js
+++ assets/script.js
@@ -5,19 +5,50 @@ let apiKey = "fc260512ce40c86fc05bd85accb60887";
 
 // DOM elements
 let searchForm = document.querySelector("#search-form");
-let searchInputVal = document.querySelector("#search-input").value;
+let searchInputVal = document.querySelector("#search-input");
+let currentForecast = document.querySelector("#current-forecast");
+let cityName = document.querySelector("#city-name");
+let cityTemp = document.querySelector("#temp");
+let cityWind = document.querySelector("#wind");
+let cityHumidity = document.querySelector("#humid");
 
-// Search input and button function
-function searchFormSubmit(event) {
-    event.preventDefault();// prevents page from loading after clicking.
 
-    searchInputVal.value;// the text inputed in the search bar 
-    console.log(searchInputVal); // checks that the input value works
-    if (searchInputVal !== null) {
+
+function searchForecast (event){
+
+    event.preventDefault();
+
+    console.log("input", searchInputVal.value);
+    console.log("api key", apiKey);
+
+    if (searchInputVal.value.trim() == null) {
         alert("Please Enter a City Name!");// alert for when the search button is pressed with no city name.
         return;
     }
 
+
+    let weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+searchInputVal.value.trim()+"&appid="+ apiKey;
+
+
+    fetch (weatherApiUrl)
+        .then (response =>{
+            if(!response.ok){
+                throw new ERROR("Network reponse was not okay");
+            }
+            return response.json();
+        })
+
+        .then(data => {
+            currentForecast.textContent = JSON.stringify(data, null, 2);
+            console.log(data);
+        })
+
+        .catch(error =>{
+            console.error("Error: ", error)
+        })
+
 }
 
-searchForm.addEventListener('submit', searchFormSubmit);//event listener for the search form button
+// Search input and button function
+
+searchForm.addEventListener('submit', searchForecast);//event listener for the search form button
