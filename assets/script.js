be78ae0270befa70beb749778a77b2af57027367
Alexis Merino <alexismerino49@gmail.com>
2024-03-12 13:56:55 -0500

    addded local storage and rendered the cities inputted as search history. Also doubled checked API Calls for functionality

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ let apiKey = "fc260512ce40c86fc05bd85accb60887";
 
 // DOM elements
 let searchForm = document.querySelector("#search-form");
-let searchInputVal = document.querySelector("#search-input");
+let searchInput = document.querySelector("#search-input");
 // current weather DOM elements
 let currentForecast = document.querySelector("#current-forecast");
 let cityName = document.querySelector("#city-name");
@@ -39,28 +39,32 @@ let fortemp4 = document.querySelector("#temp4");
 let forwind4 = document.querySelector("#win4");
 let forhum4 = document.querySelector("#humid4");
 
-
+let cities = [];
 
 // searches for the city name through the weather API
-function searchForecast (event){
+function searchForecast(event) {
 
     event.preventDefault();
+    let citiesInput = searchInput.value.trim();
 
-    console.log("input", searchInputVal.value);
-    
+    console.log("input", searchInput.value);
 
-    if (searchInputVal.value.trim() == null) {
+
+    if (citiesInput == "") {
         alert("Please Enter a City Name!");// alert for when the search button is pressed with no city name.
         return;
     }
 
-// current weather api call
-    let weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+searchInputVal.value.trim()+"&appid="+ apiKey;
-    
+    cities.push(citiesInput);
+    searchInput.value = "";
 
-    fetch (weatherApiUrl)
-        .then (response =>{
-            if(!response.ok){
+    // current weather api call
+    let weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + citiesInput + "&appid=" + apiKey;
+
+
+    fetch(weatherApiUrl)
+        .then(response => {
+            if (!response.ok) {
                 throw new ERROR("Network reponse was not okay");
             }
             return response.json();
@@ -69,111 +73,153 @@ function searchForecast (event){
         })
 
         .then(data => {
-            let curricon = "https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
-            
-        //  adds the data in the api to the html 
-            let date =new Date((data.dt)*1000);
-            cityName.innerHTML = data.name+" "+date.toLocaleDateString('en-US')+ "<img src ="+curricon+">"; // adds the name of the city, current day, and weather icon
-            cityHumidity.innerHTML = data.main.humidity+"%";// current humidity
+            let curricon = "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
 
-            let  fahrenheit = (data.main.temp - 273.15) * 1.80 + 32// converts kelvin to fahrenheit
-            cityTemp.innerHTML= fahrenheit.toFixed(2)+ "&#8457;";// adds the fahreheit to the html with its symbol.
-            
+            //  adds the data in the api to the html 
+            let date = new Date((data.dt) * 1000);
+            cityName.innerHTML = data.name + " " + date.toLocaleDateString('en-US') + "<img src =" + curricon + ">"; // adds the name of the city, current day, and weather icon
+            cityHumidity.innerHTML = data.main.humidity + "%";// current humidity
 
-            let mph = (data.wind.speed)* 2.237; // gets meters per sec and mutiplies its to convert it to MPH
-            cityWind.innerHTML = mph.toFixed(1)+" MPH";// adds the mph to the html with MPH
-    
+            let fahrenheit = (data.main.temp - 273.15) * 1.80 + 32// converts kelvin to fahrenheit
+            cityTemp.innerHTML = fahrenheit.toFixed(2) + "&#8457;";// adds the fahreheit to the html with its symbol.
+
+
+            let mph = (data.wind.speed) * 2.237; // gets meters per sec and mutiplies its to convert it to MPH
+            cityWind.innerHTML = mph.toFixed(1) + " MPH";// adds the mph to the html with MPH
+
+
             console.log("current weather", data);
         })
 
-        .catch(error =>{
+        .catch(error => {
             console.error("Error: ", error)
         })
 
-        
- // forcast api call
-    let forecastApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+searchInputVal.value.trim()+"&appid="+ apiKey;
 
-    fetch (forecastApiUrl)
-    .then (response =>{
-        if(!response.ok){
-            throw new ERROR("Network reponse was not okay");
-        }
-        return response.json();
-    })
-    .then(data => {
-        
-    //day 1
-        let foredate0 = new Date((data.list[((0+1)*8)-1].dt)*1000);
-        let foreicons0 = "https://openweathermap.org/img/wn/"+data.list[0].weather[0].icon+".png";
-        let foreFahrenheit0 = ((data.list[((0+1)*8)-1].main.temp - 273.15)* 1.80 + 32).toFixed(2); 
-        let forehumid0 =data.list[((0+1)*8)-1].main.humidity+"%";
-        let foreMph0= ((data.list[((0+1)*8)-1].wind.speed )* 2.237).toFixed(1);
+    // forcast api call
+    let forecastApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + citiesInput + "&appid=" + apiKey;
 
-        fordate0.innerHTML = foredate0.toLocaleDateString('en-US') + "<img src ="+foreicons0+">";
-        fortemp0.innerHTML = foreFahrenheit0 + " &#8457;";
-        forwind0.innerHTML = foreMph0 +" MPH";
-        forhum0.innerHTML = forehumid0;
-    // day 2
-        let foredate1 = new Date((data.list[((1+1)*8)-1].dt)*1000);
-        let foreicons1= "https://openweathermap.org/img/wn/"+data.list[1].weather[0].icon+".png";
-        let foreFahrenheit1 = ((data.list[((0+1)*8)-1].main.temp - 273.15)* 1.80 + 32).toFixed(2); 
-        let forehumid1 =data.list[((1+1)*8)-1].main.humidity+"%";
-        let foreMph1= ((data.list[((1+1)*8)-1].wind.speed )* 2.237).toFixed(1)
+    fetch(forecastApiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new ERROR("Network reponse was not okay");
+            }
+            return response.json();
+        })
+        .then(data => {
 
-        fordate1.innerHTML = foredate1.toLocaleDateString('en-US') + "<img src ="+foreicons1+">";
-        fortemp1.innerHTML = foreFahrenheit1 + " &#8457;";
-        forwind1.innerHTML = foreMph1 +" MPH";
-        forhum1.innerHTML = forehumid1;
-    // day 3
-    let foredate2 = new Date((data.list[((2+1)*8)-1].dt)*1000);
-    let foreicons2= "https://openweathermap.org/img/wn/"+data.list[2].weather[0].icon+".png";
-    let foreFahrenheit2 = ((data.list[((2+1)*8)-1].main.temp - 273.15)* 1.80 + 32).toFixed(2); 
-    let forehumid2 =data.list[((2+1)*8)-1].main.humidity+"%";
-    let foreMph2= ((data.list[((2+1)*8)-1].wind.speed )* 2.237).toFixed(1)
+            //day 1
+            let foredate0 = new Date((data.list[((0 + 1) * 8) - 1].dt) * 1000);
+            let foreicons0 = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png";
+            let foreFahrenheit0 = ((data.list[((0 + 1) * 8) - 1].main.temp - 273.15) * 1.80 + 32).toFixed(2);
+            let forehumid0 = data.list[((0 + 1) * 8) - 1].main.humidity + "%";
+            let foreMph0 = ((data.list[((0 + 1) * 8) - 1].wind.speed) * 2.237).toFixed(1);
 
-    fordate2.innerHTML = foredate2.toLocaleDateString('en-US') + "<img src ="+foreicons2+">";
-    fortemp2.innerHTML = foreFahrenheit2 + " &#8457;";
-    forwind2.innerHTML = foreMph2 +" MPH";
-    forhum2.innerHTML = forehumid2;
-    // // day 4
-    let foredate3 = new Date((data.list[((3+1)*8)-1].dt)*1000);
-    let foreicons3= "https://openweathermap.org/img/wn/"+data.list[3].weather[0].icon+".png";
-    let foreFahrenheit3 = ((data.list[((3+1)*8)-1].main.temp - 273.15)* 1.80 + 32).toFixed(2); 
-    let forehumid3 =data.list[((3+1)*8)-1].main.humidity+"%";
-    let foreMph3= ((data.list[((3+1)*8)-1].wind.speed )* 2.237).toFixed(1)
+            fordate0.innerHTML = foredate0.toLocaleDateString('en-US') + "<img src =" + foreicons0 + ">";
+            fortemp0.innerHTML = foreFahrenheit0 + " &#8457;";
+            forwind0.innerHTML = foreMph0 + " MPH";
+            forhum0.innerHTML = forehumid0;
+            // day 2
+            let foredate1 = new Date((data.list[((1 + 1) * 8) - 1].dt) * 1000);
+            let foreicons1 = "https://openweathermap.org/img/wn/" + data.list[1].weather[0].icon + ".png";
+            let foreFahrenheit1 = ((data.list[((0 + 1) * 8) - 1].main.temp - 273.15) * 1.80 + 32).toFixed(2);
+            let forehumid1 = data.list[((1 + 1) * 8) - 1].main.humidity + "%";
+            let foreMph1 = ((data.list[((1 + 1) * 8) - 1].wind.speed) * 2.237).toFixed(1)
 
-    fordate3.innerHTML = foredate3.toLocaleDateString('en-US') + "<img src ="+foreicons3+">";
-    fortemp3.innerHTML = foreFahrenheit3 + " &#8457;";
-    forwind3.innerHTML = foreMph3 +" MPH";
-    forhum3.innerHTML = forehumid3;
-    // // day 5
-    let foredate4 = new Date((data.list[((4+1)*8)-1].dt)*1000);
-    let foreicons4= "https://openweathermap.org/img/wn/"+data.list[4].weather[0].icon+".png";
-    let foreFahrenheit4 = ((data.list[((4+1)*8)-1].main.temp - 273.15)* 1.80 + 32).toFixed(2); 
-    let forehumid4 =data.list[((4+1)*8)-1].main.humidity+"%";
-    let foreMph4= ((data.list[((4+1)*8)-1].wind.speed )* 2.237).toFixed(1)
+            fordate1.innerHTML = foredate1.toLocaleDateString('en-US') + "<img src =" + foreicons1 + ">";
+            fortemp1.innerHTML = foreFahrenheit1 + " &#8457;";
+            forwind1.innerHTML = foreMph1 + " MPH";
+            forhum1.innerHTML = forehumid1;
+            // day 3
+            let foredate2 = new Date((data.list[((2 + 1) * 8) - 1].dt) * 1000);
+            let foreicons2 = "https://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + ".png";
+            let foreFahrenheit2 = ((data.list[((2 + 1) * 8) - 1].main.temp - 273.15) * 1.80 + 32).toFixed(2);
+            let forehumid2 = data.list[((2 + 1) * 8) - 1].main.humidity + "%";
+            let foreMph2 = ((data.list[((2 + 1) * 8) - 1].wind.speed) * 2.237).toFixed(1)
 
-    fordate4.innerHTML = foredate4.toLocaleDateString('en-US') + "<img src ="+foreicons4+">";
-    fortemp4.innerHTML = foreFahrenheit4 + " &#8457;";
-    forwind4.innerHTML = foreMph4 +" MPH";
-    forhum4.innerHTML = forehumid4;
-        
-        
+            fordate2.innerHTML = foredate2.toLocaleDateString('en-US') + "<img src =" + foreicons2 + ">";
+            fortemp2.innerHTML = foreFahrenheit2 + " &#8457;";
+            forwind2.innerHTML = foreMph2 + " MPH";
+            forhum2.innerHTML = forehumid2;
+            // // day 4
+            let foredate3 = new Date((data.list[((3 + 1) * 8) - 1].dt) * 1000);
+            let foreicons3 = "https://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + ".png";
+            let foreFahrenheit3 = ((data.list[((3 + 1) * 8) - 1].main.temp - 273.15) * 1.80 + 32).toFixed(2);
+            let forehumid3 = data.list[((3 + 1) * 8) - 1].main.humidity + "%";
+            let foreMph3 = ((data.list[((3 + 1) * 8) - 1].wind.speed) * 2.237).toFixed(1)
+
+            fordate3.innerHTML = foredate3.toLocaleDateString('en-US') + "<img src =" + foreicons3 + ">";
+            fortemp3.innerHTML = foreFahrenheit3 + " &#8457;";
+            forwind3.innerHTML = foreMph3 + " MPH";
+            forhum3.innerHTML = forehumid3;
+            // // day 5
+            let foredate4 = new Date((data.list[((4 + 1) * 8) - 1].dt) * 1000);
+            let foreicons4 = "https://openweathermap.org/img/wn/" + data.list[4].weather[0].icon + ".png";
+            let foreFahrenheit4 = ((data.list[((4 + 1) * 8) - 1].main.temp - 273.15) * 1.80 + 32).toFixed(2);
+            let forehumid4 = data.list[((4 + 1) * 8) - 1].main.humidity + "%";
+            let foreMph4 = ((data.list[((4 + 1) * 8) - 1].wind.speed) * 2.237).toFixed(1)
+
+            fordate4.innerHTML = foredate4.toLocaleDateString('en-US') + "<img src =" + foreicons4 + ">";
+            fortemp4.innerHTML = foreFahrenheit4 + " &#8457;";
+            forwind4.innerHTML = foreMph4 + " MPH";
+            forhum4.innerHTML = forehumid4;
 
 
-        
-        console.log("forecast",data);
-    })
+            console.log("forecast", data);
+        })
 
 
 
-    .catch(error =>{
-        console.error("Error: ", error)
-    })
+        .catch(error => {
+            console.error("Error: ", error)
+        })
 
+    storeCitites();
+    renderSearchHistory();
 }
 
 // Search input and button function
 
 searchForm.addEventListener('submit', searchForecast);//event listener for the search form button
+
+// function to add cities to search history
+function renderSearchHistory() {
+    let history = document.querySelector("#search-history");
+    history.innerHTML = "";
+    
+    for (i = 0; i < cities.length; i++) {
+        let city = cities[i];
+
+        let list = document.createElement("ol");
+        list.clas
+        list.textContent = city;
+        list.dataset.index = i;
+
+        history.appendChild(list);
+
+    }
+
+}
+
+
+function storedCitites() {
+    //get the stored cities from local storage
+    let savedCitites = JSON.parse(localStorage.getItem("cities"));
+
+    if (savedCitites !== null) {
+        cities = savedCitites;
+    }
+
+    // helper function to load the citites on the HTML
+    renderSearchHistory();
+ 
+}
+// sets citites into local storage
+function storeCitites() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+
+}
+
+console.log(localStorage.getItem("cities"));
+// load the storedCities function on window load
+storedCitites();
